c6d9ccb6c6470d60b1456f061014f737b6195ab2
harshshah96 <98999838+harshshah96@users.noreply.github.com>
2023-12-24 09:42:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (86.65%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,30 +2,33 @@ static int io_opt = []() {
   ios::sync_with_stdio(false);
   return 0;
 }();
+
 class Solution {
 public:
-    int minOperations(string s) {
-        int n=s.size();
-        char ini=s[0];
-        int c1 = count(s,ini);
-        int c2= count(s, ini =='0'? '1':'0');
-        c2++;
-
-        return min(c1,c2);
-    }
-private:
-     int count(string s, char pre){
-        int count=0;
-        int n=s.size();
-        for(int i=1; i<n; i++){
-            char curr=s[i];
-            if(pre==curr){
-                count++;
-                pre = (pre == '0'? '1' : '0');
-            }else{
-                pre=curr;
+    int minOperations(string s){
+    int answer = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if ((s[i] - '0') != i % 2){
+                answer++;
             }
         }
-        return count;
+        return min(answer, static_cast<int>(s.length() - answer));
     }
+
+// private:
+//      int count(string s, char pre){
+//         int count=0;
+//         int n=s.size();
+//         for(int i=1; i<n; i++){
+//             char curr=s[i];
+//             if(pre==curr){
+//                 count++;
+//                 pre = (pre == '0'? '1' : '0');
+//             }else{
+//                 pre=curr;
+//             }
+//         }
+//         return count;
+//     }
 };
