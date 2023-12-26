f0c40ebcd9a314709b8767ecb437fca57e1d5649
harshshah96 <98999838+harshshah96@users.noreply.github.com>
2023-12-26 07:41:17 +0530

    Time: 24 ms (63.55%) | Memory: 19.6 MB (5.21%) - LeetSync

---
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,26 @@
+#define ll long long int
+#define mod 1000000007
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int f(int n, int k, int target){
+        
+        if(target==0 and n==0) return 1;
+        if(n==0) return 0;
+
+        if(dp[n][target]!=-1) return dp[n][target];
+        
+        int count=0;
+
+        for(int i=1; i<=k; i++){
+            if(target-i<0) break;
+            count = (count%mod + f(n-1, k, target-i)%mod)%mod;
+        }
+        return dp[n][target] = count%mod;
+    }
+    int numRollsToTarget(int n, int k, int target) {
+        dp.clear();
+        dp.resize(35,vector<int>(1005,-1));
+        return f(n,k,target);
+    }
+};
