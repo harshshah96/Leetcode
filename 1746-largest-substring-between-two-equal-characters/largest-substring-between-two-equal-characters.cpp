99f001d5fc9be0158660081b8f7c44d342fab605
harshshah96 <98999838+harshshah96@users.noreply.github.com>
2023-12-31 08:41:06 +0530

    Time: 7 ms (5.74%) | Memory: 6.5 MB (97.01%) - LeetSync

---
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        int n=s.length();
+        int i=0;
+        int ans=INT_MIN;
+        while(i<n){
+            int j = n-1;
+            while(j<n and s[i]!=s[j]) j--;
+            if(j<n){
+                ans=max(ans,j-i-1);
+            }
+            i++;
+        }
+
+        return ans == INT_MIN ? -1 : ans ;
+        
+    }
+};
