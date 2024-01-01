bd4e3fd0a63d9b9d738ae256af631179726a3526
harshshah96 <98999838+harshshah96@users.noreply.github.com>
2024-01-01 08:57:04 +0530

    Time: 29 ms (15.10%) | Memory: 17.9 MB (36.11%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,26 @@
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {  
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i=0;
+        int j=0;
+        int count=0;
+
+        while(i<g.size() && j<s.size()){
+            if(s[j]>=g[i]){
+                count++;
+                i++;
+                j++;
+            }
+            else j++;
+        }
+        return count;
+
+        }
+};
