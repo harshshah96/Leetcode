5fa5f161292f1f8cc196812d1043971e096b3f5d
harshshah96 <98999838+harshshah96@users.noreply.github.com>
2024-01-03 18:12:44 +0530

    Time: 8 ms (37.93%) | Memory: 8.3 MB (72.45%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+
+        stack<string> st;
+        int n=path.length();
+        int i=0;
+        while(i<n){
+            int j=i+1;
+            if(path[i]=='/') {
+                i++;
+                continue;
+
+            }
+            while(j<n && path[j]!='/') j++;
+            string s=path.substr(i,j-i);
+            if(s==".."){
+                if(!st.empty()){
+                    st.pop();
+                    
+                }
+                i=j;
+                continue;
+            } else if(s=="."){
+                i=j;
+                continue;
+            }
+            st.push(s);
+            i=j;
+        }
+        string ans="";
+        stack<string>temp;
+
+        if(st.empty()) return "/";
+
+        while(!st.empty()){
+            temp.push(st.top());
+            st.pop();
+        }
+        while(!temp.empty()){
+            string a="/";
+            a+=temp.top();
+            temp.pop();
+            ans+=a;
+        }
+
+        return ans;
+
+    
+
+        
+    }
+};
