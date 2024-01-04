51976b535449da84a3cd982b67550cbe2c4621af
harshshah96 <98999838+harshshah96@users.noreply.github.com>
2024-01-04 16:53:15 +0530

    Time: 6 ms (52.98%) | Memory: 10.9 MB (78.04%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,44 @@
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
+class Solution {
+public:
+    int target;
+
+    void dfs(vector<int>& candidates, vector<vector<int>>& ans, vector<int>& temp, int idx, int sum) {
+        if (sum == target) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int i = idx; i < candidates.size(); ++i) {
+            // Skip duplicates
+            if (i > idx && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+            // Check if adding the current candidate exceeds the target
+            if (sum + candidates[i] <= target) {
+                temp.push_back(candidates[i]);
+                dfs(candidates, ans, temp, i + 1, sum + candidates[i]);
+                temp.pop_back();
+            }
+        }
+
+        return;
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int tgt) {
+        target = tgt;
+
+        // Sort the candidates to handle duplicates properly
+        sort(candidates.begin(), candidates.end());
+
+        vector<int> temp;
+        vector<vector<int>> ans;
+        dfs(candidates, ans, temp, 0, 0);
+
+        return ans;
+    }
+};
