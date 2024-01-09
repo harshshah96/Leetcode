d0746edf553b979fbddce71c6d90c0fb2a4d4612
harshshah96 <98999838+harshshah96@users.noreply.github.com>
2024-01-09 08:51:54 +0530

    Time: 4 ms (45.42%) | Memory: 7.1 MB (37.30%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -0,0 +1,63 @@
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
+class Solution {
+private:
+    int pos[4];
+public:
+    bool check(string ip)
+    {
+        if(ip.size()==1)
+        {
+            return "0"<=ip && ip<="9";
+        }
+        if(ip.size()==2)
+        {
+            return "10"<=ip && ip<="99";
+        }
+        if(ip.size()==3)
+        {
+            return "100"<=ip && ip<="255";
+        }
+        return false;
+    }
+    
+    void dfs(string &s,int start,int dep,int maxDep,vector<string>& res)
+    {
+        if(dep==maxDep)
+        {
+            if(start==s.size())
+            {
+                int begin=0;
+                string add="";
+                for(int i=0;i<maxDep;++i)
+                {
+                    string ip(s,begin,pos[i]-begin+1);
+                    begin=pos[i]+1;
+                    add+=i==0?ip:"."+ip;
+                }
+                res.push_back(add);
+                
+            }
+            return;
+        }
+        for(int i=start;i<s.size();++i)
+        {
+            string ip(s,start,i-start+1);
+            if(check(ip))
+            {
+                pos[dep]=i;
+                dfs(s,i+1,dep+1,maxDep,res);
+            }
+        }
+    }
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> res;
+        //res.clear();
+        if(s.size()>12)
+            return res;
+        dfs(s,0,0,4,res);
+        return res;
+    }
+};
