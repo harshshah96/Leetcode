e99fb313efd316c5bdbdc269b7542e2ac9338c9c
harshshah96 <98999838+harshshah96@users.noreply.github.com>
2024-01-11 15:25:46 +0530

    Time: 17 ms (78.01%) | Memory: 28 MB (41.99%) - LeetSync

---
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,33 @@
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
+class Solution {
+public:
+
+    int maxSum;
+    int solve(TreeNode* root){
+        if(root==NULL) return 0;
+
+        int l= solve(root->left);
+        int r= solve(root->right);
+
+        int neche_wala_path = l+r+ root->val;
+        int koi_ek_acha = max(l,r) + root->val;
+        int koi_acha_nahin = root->val;
+
+        maxSum = max(maxSum, max(neche_wala_path, max(koi_ek_acha, koi_acha_nahin )));
+
+        return max(koi_ek_acha, koi_acha_nahin);
+    }
+
+    int maxPathSum(TreeNode* root) {
+        
+        maxSum=INT_MIN;
+
+        solve(root);
+
+        return maxSum;
+
+    }
+};
