8fbc16305b734e1f365ad2dafa26f5d07fde14c1
harshshah96 <98999838+harshshah96@users.noreply.github.com>
2024-01-13 09:02:33 +0530

    Time: 88 ms (33.40%) | Memory: 19.9 MB (5.16%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,20 +1,24 @@
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
 class Solution {
 public:
     int minSteps(string s, string t) {
         int  n=s.length();
 
         unordered_map<char, int> mp;
-        unordered_map<char, int> mp2;
 
         for(int i=0; i<n; i++){
             mp[s[i]]++;
-            mp2[t[i]]++;
+        }
+        for(int i=0; i<n; i++){
+            if(mp[t[i]]>0) mp[t[i]]--;
+            if(mp[t[i]]==0) mp.erase(t[i]);
         }
         int ans=0;
         for(auto ele: mp){
-            char a=ele.first;
-            int freq= ele.second;
-            if(mp2[a]<freq) ans+= (freq-mp2[a]);
+            ans+=ele.second;
 
         }
 
