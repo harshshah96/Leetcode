a158845ec1933affcc2ecfc0f7cd6b1edcde671b
harshshah96 <98999838+harshshah96@users.noreply.github.com>
2024-01-16 13:50:43 +0530

    Time: 21 ms (24.66%) | Memory: 34.9 MB (14.35%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -4,16 +4,16 @@ static int io_opt = []() {
 }();
 class Solution {
 public:
-    vector<vector<int>> dp;
-    bool solve(string s1, string s2, string s3, int i, int j, int k){
+    
+    bool solve(string s1, string s2, string s3, int i, int j, vector<vector<int>> &dp){
 
-        if(k==s3.size()) return true;
+        if(i+j==s3.size()) return true;
 
         if(dp[i][j]!=INT_MIN) return dp[i][j];
 
 
-        bool solve1= s1[i]==s3[k] ? solve(s1,s2,s3,i+1,j,k+1) : false;
-        bool solve2= s2[j]==s3[k] ? solve(s1,s2,s3,i,j+1,k+1) : false;
+        bool solve1= (i<s1.length() && s1[i]==s3[i+j]) ? solve(s1,s2,s3,i+1,j, dp) : false;
+        bool solve2= (j<s2.length() && s2[j]==s3[i+j]) ? solve(s1,s2,s3,i,j+1, dp) : false;
 
         return dp[i][j]=solve1 || solve2;
         
@@ -22,11 +22,13 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
 
         if(s3.length()!=s1.length()+s2.length()) return false;
+
+        vector<vector<int>> dp;
         dp.clear();
         dp.resize(s1.length()+1, vector<int> (s2.length()+1, INT_MIN));
 
 
-        return solve(s1,s2,s3,0,0,0);
+        return solve(s1,s2,s3,0,0,dp);
         
     }
 };
