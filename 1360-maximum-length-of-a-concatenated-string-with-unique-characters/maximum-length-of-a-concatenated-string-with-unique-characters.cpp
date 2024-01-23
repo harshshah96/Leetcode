58df945ebb659236748bbe6af53024145bf12d89
harshshah96 <98999838+harshshah96@users.noreply.github.com>
2024-01-23 14:51:33 +0530

    Time: 45 ms (60.70%) | Memory: 57 MB (49.12%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool hasDuplicates(string &temp, string &b){
+        vector<int> check(26,0);
+        for(int i=0; i<temp.length(); i++){
+            if(check[temp[i]-'a']==0) check[temp[i]-'a'] ++;
+            else return true; 
+        }
+        for(int i=0; i<b.length(); i++){
+            if(check[b[i]-'a']==0) check[b[i]-'a'] ++;
+            else return true; 
+        }
+
+        return false;
+    }
+
+    int solve(vector<string> &arr, int idx, string &temp){
+        if(idx==arr.size()) return temp.length();
+        string a=arr[idx];
+        bool flag= hasDuplicates(a, temp);
+
+        int include=INT_MIN;
+        int exclude=INT_MIN;
+
+        if(!flag){
+            string b=temp;
+            b+=a;
+            include=solve(arr,idx+1, b);
+        }
+        exclude=solve(arr, idx+1, temp);
+
+        return max(include,exclude);
+        
+        
+    }
+
+    int maxLength(vector<string>& arr) {
+        int n=arr.size();
+
+        string a="";
+        return solve(arr,0,a);
+
+
+
+
+        
+    }
+};
