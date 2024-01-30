9ac8e13f70fd10a65733a165fea33e73f6df5661
Harsh Shah <shahharsh987656@gmail.com>
2024-01-30 10:05:48 +0530

    Time: 11 ms (40.49%) | Memory: 15.6 MB (5.26%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,45 @@
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
+class Solution {
+public:
+    int f(int first, int second, string op){
+        if(op=="*") return second*first;
+        else if(op == "+") return second+first;
+        else if(op == "-") return second - first;
+        else if(op == "/") {
+            int a=second/first;
+            if(a<0){
+                if(a*first==second)  return a;
+                else return a;
+            }
+            else return a ;
+        }
+
+        return -1;
+    }
+
+    int evalRPN(vector<string>& tokens) {
+        int n=tokens.size();
+        stack<int>st;
+
+        int i=0;
+        while(i<n){
+            if(tokens[i]=="*" || tokens[i]=="-" || tokens[i]=="/" || tokens[i]=="+"){
+                int first=(st.top());
+                st.pop();
+                int second=(st.top());
+                st.pop();
+
+                int result = f(first,second,tokens[i]);
+                st.push(result);
+            }else{
+                st.push(stoi(tokens[i]));
+            }
+            i++;
+
+        }
+        return st.top();
+    }
+};
