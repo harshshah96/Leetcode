fd2ee3f156182fae0a0604f4a2b53cfafd7c8d5f
Harsh Shah <shahharsh987656@gmail.com>
2024-02-17 17:04:49 +0530

    Update furthest-building-you-can-reach.java

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
@@ -1,8 +1,9 @@
 import java.util.PriorityQueue;
 
 class Solution {
-    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) 
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        // (a, b) -> b - a: This is a lambda expression that defines a custom comparator for the priority queue. It takes two Integer values a and b and returns the result of subtracting a from b. In other words, it defines a comparator that sorts elements in descending order (largest first).
         int bricksUsed = 0;
         int i = 0;
         for (; i < heights.length - 1; i++) {
