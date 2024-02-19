a0a9afef34c25ff7447e935b656746048b050492
Harsh Shah <shahharsh987656@gmail.com>
2024-02-19 09:17:42 +0530

    Time: 6 ms (20.09%) | Memory: 7.2 MB (19.05%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -5,6 +5,8 @@ static int io_opt = []() {
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        if(n==1) return true;
+        if(n%2!=0) return false;
         int count=0;
         while(n>0){
             count+=(n&1);
