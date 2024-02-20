ae31fb804baf325d33d0240bf7b3845a3631208a
Harsh Shah <shahharsh987656@gmail.com>
2024-02-20 11:32:24 +0530

    Time: 57 ms (9.89%) | Memory: 21.7 MB (6.08%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -12,8 +12,11 @@ public:
             while(i<n && nums[i]!=0) i++;
             j=i+1;
             while(j<n && nums[j]==0) j++;
-
-            if(i<n && j<n)swap(nums[i],nums[j]);
+            if(i<n && j<n){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
             i++;
         }
     }
