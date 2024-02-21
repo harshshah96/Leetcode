afaf724cca1ec9c145584b11ea39c220e602f2d3
Harsh Shah <shahharsh987656@gmail.com>
2024-02-21 10:51:49 +0530

    Time: 20 ms (19.05%) | Memory: 8.8 MB (46.79%) - LeetSync

---
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,16 @@
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int count=0;
+        while(left!=right){
+            left=left>>1;
+            right=right>>1;
+            count++;
+        }
+        return(left<<count);
+    }
+};
