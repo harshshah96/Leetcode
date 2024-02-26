59da895eb4725ff9a56d6f5c1fefc0ecc644d7bd
Harsh Shah <shahharsh987656@gmail.com>
2024-02-26 13:11:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (25.40%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -7,7 +7,7 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL && q==NULL) return true;
         if((p==NULL && q!=NULL) || (p!=NULL && q==NULL)) return false;
-
+        
         if(p->val != q->val) return false;
 
         bool leftAns= isSameTree(p->left, q->left);
@@ -15,7 +15,6 @@ public:
 
         bool rightAns= isSameTree(p->right, q->right);
         if(rightAns==false) return false;
-
         return true;
     }
 };
