a037ceeb143a13882243816087a2ce55a0a4452b
Harsh Shah <shahharsh987656@gmail.com>
2024-02-29 23:37:00 +0530

    Time: 132 ms (100.00%) | Memory: 149.3 MB (69.10%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.cpp
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -15,24 +15,19 @@ public:
         while(!q.empty()){
             int n=q.size();
             int prev=q.front()->val;
+
             for(int i=0; i<n; i++){
                 TreeNode* temp = q.front();
-                if(i>=1) {
-                    if(level%2==0){//even
-                        if(temp->val <=prev) return false;
-                    }else{
-                        if(temp->val >= prev) return false;
-                    }
 
+                if(i>=1) { 
+                    if((level%2==0 && temp->val <=prev) || (level%2 !=0 && temp->val >=prev)) return false;
                     prev=temp->val;
                 }
-                if(level%2==0 && temp->val % 2 ==0 || level %2 !=0 && temp->val % 2 !=0) return false;
+                if((level%2==0 && temp->val % 2 ==0) || (level %2 !=0 && temp->val % 2 !=0)) return false;
                 q.pop();
                 if(temp->left) q.push(temp->left);
                 if(temp->right) q.push(temp->right);
-                cout<< temp->val<<" ";
             }
-            cout<<endl;
             level++;
         }
 
