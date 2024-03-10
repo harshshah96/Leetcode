6ea16bbac54ec465010c8840340e0d4e438e297c
Harsh Shah <shahharsh987656@gmail.com>
2024-03-10 11:02:41 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (32.08%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,28 +1,20 @@
-static int io_opt = []() {
-  ios::sync_with_stdio(false);
-  return 0;
-}();
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        // approach 1: using hash-sets
-        unordered_set<int> set1;
-        if(nums1.size()>nums2.size()) return intersection(nums2,nums1);
-        for(int ele : nums1){
-            set1.insert(ele);
+        unordered_map<int,int> um;
+        vector<int> res;
+        for(int i = 0; i < nums1.size(); i++)
+        {
+            um[nums1[i]]++;
         }
-        vector<int>ans;
-        for(int ele : nums2){
-            if(set1.count(ele)){
-                ans.push_back(ele);
-                set1.erase(ele);
-
+        for(int i = 0;i < nums2.size(); i++)
+        {
+            if(um.find(nums2[i]) != um.end())
+            {
+                res.push_back(nums2[i]);
+                um.erase(nums2[i]);
             }
         }
-        return ans;
-
-
-
-        
+        return res;
     }
 };
