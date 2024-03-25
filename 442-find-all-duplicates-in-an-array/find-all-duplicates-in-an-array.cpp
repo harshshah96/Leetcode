a83bf9bcaabab265193e78fedaab4d734fd3619f
Harsh Shah <shahharsh987656@gmail.com>
2024-03-25 10:25:54 +0530

    Time: 18 ms (99.81%) | Memory: 35.8 MB (98.03%) - LeetSync

---
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,24 @@
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+
+        int n=nums.size();
+        vector<int> a;
+        int i=0;
+        while(i<n){
+            int correctIdx=nums[i]-1;
+            if(nums[i]==i+1 || nums[i]==nums[correctIdx])i++;
+            else swap(nums[i],nums[correctIdx]);
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i]!= i+1) a.push_back(nums[i]);
+        }
+
+        return a;
+        
+    }
+};
