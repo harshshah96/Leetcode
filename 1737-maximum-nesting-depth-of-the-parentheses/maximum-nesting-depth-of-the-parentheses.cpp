59543bd7b939d042aa335b80f3291426e0ca55ad
Harsh Shah <shahharsh987656@gmail.com>
2024-04-04 14:44:44 +0530

    Time: 4 ms (10.43%) | Memory: 7.4 MB (78.96%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,18 @@
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth=0;
+        int maxDepth=0;
+        for(int i=0; i<s.length(); i++){
+            if(s[i]=='(') depth +=1;
+            else if(s[i]== ')') depth-=1;
+            maxDepth=max(maxDepth,depth);
+        }
+
+        return maxDepth;
+    }
+};
