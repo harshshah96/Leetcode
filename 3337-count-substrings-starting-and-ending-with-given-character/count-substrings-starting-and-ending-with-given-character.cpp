53a366196665afd2d4e6ce06cbde8236f598670c
Harsh Shah <shahharsh987656@gmail.com>
2024-04-10 19:37:02 +0530

    Time: 7 ms (94.04%) | Memory: 11.2 MB (37.32%) - LeetSync

--- 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.cpp
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.cpp
@@ -1,9 +1,11 @@
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
 class Solution {
 public:
     long long countSubstrings(string s, char c) {
         int n=s.length();
-
-        int i=0;
         long long int count=0;
 
         for(int i=0; i<n; i++){
