0ed794a5c4727404ae89f6f44401f3116903b483
Harsh Shah <shahharsh987656@gmail.com>
2024-04-13 22:09:46 +0530

    Time: 22 ms (96.79%) | Memory: 17.4 MB (75.60%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -4,32 +4,46 @@ static int io_opt = []() {
 }();
 class Solution {
 public:
+    int largestRectangleArea(vector < int > & histo) {
+	stack < int > st;
+	int maxA = 0;
+	int n = histo.size();
+	for (int i = 0; i <= n; i++) {
+		while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
+			int height = histo[st.top()];
+			st.pop();
+			int width;
+			if (st.empty())
+				width = i;
+			else
+				width = i - st.top() - 1;
+			maxA = max(maxA, width * height);
+		}
+		st.push(i);
+	}
+	return maxA;
+}
+
+int solve(vector<vector<char>>&mat, int n, int m) {
+	int maxArea = 0;
+	vector<int> height(m, 0);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (mat[i][j] == '1') height[j]++;
+			else height[j] = 0;
+		}
+		int area = largestRectangleArea(height);
+		maxArea = max(area, maxArea);
+	}
+
+	return maxArea;
+}
+
+
     int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty() || matrix[0].empty())
-            return 0;
-        
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        vector<int> heights(cols + 1, 0); // Include an extra element for easier calculation
-        int maxArea = 0;
-        
-        for (const auto& row : matrix) {
-            for (int i = 0; i < cols; i++) {
-                heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
-            }
-            
-            // Calculate max area using histogram method
-            int n = heights.size(); // Number of bars in the histogram
-            
-            for (int i = 0; i < n; i++) {
-                for (int j = i, minHeight = INT_MAX; j < n; j++) {
-                    minHeight = min(minHeight, heights[j]);
-                    int area = minHeight * (j - i + 1);
-                    maxArea = max(maxArea, area);
-                }
-            }
-        }
-        
-        return maxArea;
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        return solve(matrix, n, m);
     }
 };
