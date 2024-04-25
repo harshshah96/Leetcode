496e8da754a542630b680c7898770d88e507bc06
Harsh Shah <shahharsh987656@gmail.com>
2024-04-26 01:01:59 +0530

    Time: 259 ms (44.96%) | Memory: 11.4 MB (84.45%) - LeetSync

---
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestIdealString(string s, int k) {
+        vector<int> t(26,0);
+        int result=0;
+        for(int i=0; i<s.length(); i++){
+
+            int idx=s[i]-'a';
+            int left=max(0, idx-k);
+            int right=min(25,idx+k);
+
+            int maxVal=0;
+            for(int j=left; j<=right; j++){
+                maxVal=max(maxVal, t[j]);
+            }
+            t[idx]= maxVal+1 ;
+            result=max(result,t[idx]);
+            cout<<t[idx]<<" ";
+        }
+
+        return result;
+    }
+};
