043eb464ff2c28d9f9997e6abde3d7eef3e491e1
Harsh Shah <shahharsh987656@gmail.com>
2024-04-30 18:44:27 +0530

    Time: 433 ms (22.16%) | Memory: 20.6 MB (15.57%) - LeetSync

---
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
@@ -0,0 +1,28 @@
+typedef long long ll;
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        int n=word.length();
+
+        unordered_map<ll, ll> mp;
+        mp[0]=1;
+
+        int cum_xor=0;
+        ll  result=0;
+        for(int i=0; i<n; i++){
+
+            cum_xor ^= (1 << (word[i]-'a'));
+
+            result += mp[cum_xor];
+
+            for(int j=0 ; j<10 ; j++){
+                int c = cum_xor ^ ( 1 << j ) ;
+                result+=mp[c];
+            }
+        
+            mp[cum_xor]++;
+        }
+        
+        return result;
+    }
+};
