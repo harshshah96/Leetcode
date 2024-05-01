a9a47d49b9d1edfef6622b90605faa4030df1035
Harsh Shah <shahharsh987656@gmail.com>
2024-05-01 10:31:57 +0530

    Time: 3 ms (42.21%) | Memory: 7.8 MB (8.46%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -0,0 +1,27 @@
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        string a="";
+        int i;
+        bool flag=false;
+        for(i=0; i<word.length(); i++){
+            a+=word[i];
+            if(word[i]==ch) {
+                flag=true;
+                break;
+            }
+            
+        } 
+        if(flag){
+            reverse(a.begin(),a.end());
+            a+=word.substr(i+1,word.length()-(i+1));    
+            return a;
+        }
+        return word;
+        
+    }
+};
