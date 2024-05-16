f8de5c07700534d5c70183ed1e1ff2b2555551c5
Harsh Shah <shahharsh987656@gmail.com>
2024-05-16 10:40:48 +0530

    Time: 7 ms (87.42%) | Memory: 17.3 MB (64.84%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -1,17 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
- 
-
 class Solution {
 public:
     // 0 --> FALSE
@@ -19,19 +5,29 @@ public:
     // 2 --> OR
     // 3 --> AND
 
-    bool helper(TreeNode* root) {
-        if(root -> val == 0 || root -> val == 1) {
-            return root -> val == 1; 
-        } else if(root -> val == 2) {
-            return helper(root -> left) || helper(root -> right);
-        } else if(root -> val == 3) {
-            return helper(root -> left) && helper(root -> right);
-        } 
+    // bool helper(TreeNode* root) {
+    //     if(root -> val == 0 || root -> val == 1) {
+    //         return root -> val == 1; 
+    //     } else if(root -> val == 2) {
+    //         return helper(root -> left) || helper(root -> right);
+    //     } else if(root -> val == 3) {
+    //         return helper(root -> left) && helper(root -> right);
+    //     } 
 
-        return false;
-    }
+    //     return false;
+    // }
 
     bool evaluateTree(TreeNode* root) {
-        return helper(root);
+       if(root->val == 1)
+            return true;
+        if(root->val == 0)
+            return false;
+        if(root->val == 2)
+            return evaluateTree(root->left) or evaluateTree(root->right);
+        
+        if(root->val == 3)
+            return evaluateTree(root->left) and evaluateTree (root->right);
+
+        return true;
     }
 };
