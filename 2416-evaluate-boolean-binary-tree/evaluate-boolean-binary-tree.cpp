3afd3bd6268362d0d34bce8a1bf263b6c1a8ee82
Harsh Shah <shahharsh987656@gmail.com>
2024-05-16 10:42:50 +0530

    Time: 11 ms (56.68%) | Memory: 17.4 MB (64.84%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -6,16 +6,15 @@ public:
     // 3 --> AND
 
     bool evaluateTree(TreeNode* root) {
-       if(root->val == 1)
-            return true;
         if(root->val == 0)
             return false;
-        if(root->val == 2)
+        else if(root->val == 1)
+            return true;
+        else if(root->val == 2)
             return evaluateTree(root->left) or evaluateTree(root->right);
-        
-        if(root->val == 3)
-            return evaluateTree(root->left) and evaluateTree (root->right);
+        else return evaluateTree(root->left) and evaluateTree (root->right);
+            
 
-        return false;
+        return false; // will never be excecuted
     }
 };
