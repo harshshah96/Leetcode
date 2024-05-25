417ee8a76396683ddb08af767582348976e2d3dc
Harsh Shah <shahharsh987656@gmail.com>
2024-05-25 11:34:40 +0530

    Time: 4 ms (32.03%) | Memory: 9.4 MB (37.41%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        return wordBreakHelper(s, 0, dict);
+    }
+
+private:
+    vector<string> wordBreakHelper(string s, int i, unordered_set<string>& dict) {
+        vector<string> ans;
+        if (i == s.length())
+            ans.push_back("");
+
+        for (int j = i + 1; j <= s.length(); j++) {
+            string prefix = s.substr(i, j - i);
+            if (dict.count(prefix)) {
+                vector<string> suffixes = wordBreakHelper(s, j, dict);
+                for (string suffix : suffixes)
+                    ans.push_back(prefix + (suffix.empty() ? "" : " ") + suffix);
+            }
+        }
+        return ans;
+    }
+};
