f290bd9749aebcdfd575ca602b551ea98fb620a5
Harsh Shah <shahharsh987656@gmail.com>
2024-05-26 18:09:50 +0530

    Time: 1593 ms (5.12%) | Memory: 507.7 MB (5.12%) - LeetSync

---
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    long long solve(int n , int con_late, int abs, vector<vector<vector<int>>> &dp){
+        if(con_late > 2 || abs>1) return 0;
+        if(n<=0) return 1;
+
+        if(dp[n][abs][con_late]!=-1) return dp[n][abs][con_late];
+        long long absent = (solve(n-1,0,abs+1,dp))%1000000007;
+        long long late = (solve(n-1,con_late+1,abs,dp))%1000000007;
+        long long present = solve(n-1,0,abs,dp)%1000000007;
+
+        return dp[n][abs][con_late]=(((absent)%1000000007) + ((late)%1000000007) + ((present)%1000000007)) % (1000000007);
+
+
+    }
+    int checkRecord(int n) {
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(3,vector<int>(3,-1)));
+        return solve(n,0,0,dp);
+        
+    }
+};
