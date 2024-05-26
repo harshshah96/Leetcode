58fc60e0677aae7c23ee419fec3da4d2359185c9
Harsh Shah <shahharsh987656@gmail.com>
2024-05-26 18:15:17 +0530

    Time: 1527 ms (7.76%) | Memory: 507.9 MB (5.12%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -2,19 +2,20 @@
 static int io_opt = []() {
   ios::sync_with_stdio(false);
   return 0;
+
 }();class Solution {
 public:
-
-    long long solve(int n , int con_late, int abs, vector<vector<vector<int>>> &dp){
+    int M = 1000000007;
+    int solve(int n , int con_late, int abs, vector<vector<vector<int>>> &dp){
         if(con_late > 2 || abs>1) return 0;
-        if(n<=0) return 1;
+        if(n==0) return 1;
 
         if(dp[n][abs][con_late]!=-1) return dp[n][abs][con_late];
-        long long absent = (solve(n-1,0,abs+1,dp))%1000000007;
-        long long late = (solve(n-1,con_late+1,abs,dp))%1000000007;
-        long long present = solve(n-1,0,abs,dp)%1000000007;
+        int A = (solve(n-1,0,abs+1,dp))%1000000007;
+        int L = (solve(n-1,con_late+1,abs,dp))%1000000007;
+        int P = solve(n-1,0,abs,dp)%1000000007;
 
-        return dp[n][abs][con_late]=(((absent)%1000000007) + ((late)%1000000007) + ((present)%1000000007)) % (1000000007);
+        return dp[n][abs][con_late]=((A + L)%M + P) % M;
 
 
     }
