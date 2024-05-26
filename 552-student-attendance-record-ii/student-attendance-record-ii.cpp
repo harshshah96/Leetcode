d4984578341c1352d88fc0cbbce71de8bbd1a35f
Harsh Shah <shahharsh987656@gmail.com>
2024-05-26 18:41:41 +0530

    Time: 24 ms (91.61%) | Memory: 9.7 MB (88.82%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -11,17 +11,35 @@ public:
         if(n==0) return 1;
 
         if(dp[n][abs][con_late]!=-1) return dp[n][abs][con_late];
-        int A = (solve(n-1,0,abs+1,dp))%1000000007;
-        int L = (solve(n-1,con_late+1,abs,dp))%1000000007;
-        int P = solve(n-1,0,abs,dp)%1000000007;
+        int A = solve(n-1,0,abs+1,dp);
+        int L = solve(n-1,con_late+1,abs,dp);
+        int P = solve(n-1,0,abs,dp);
 
         return dp[n][abs][con_late]=((A + L)%M + P) % M;
 
 
     }
     int checkRecord(int n) {
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(3,vector<int>(3,-1)));
-        return solve(n,0,0,dp);
-        
+        int t[n+1][2][3];
+
+        for(int A = 0 ; A<=1; A++){
+            for(int L = 0;  L<=2 ; L++){
+                t[0][A][L] = 1;
+            }
+        }
+        for(int i=1;i<=n; i++){
+            for(int A = 0; A<=1; A++){
+                for(int L = 0; L<=2; L++){
+                    // long result=0;
+                    long result = t[i-1][A][0] ; // present
+                    result += ( L+1<=2 ? t[i-1][A][L+1] : 0 ); // Late
+                    result += ( A+1<=1 ? t[i-1][A+1][0] : 0 ); // Absent
+                    
+                    t[i][A][L] = result % M;
+                }
+            }
+        }
+
+        return t[n][0][0];
     }
 };
