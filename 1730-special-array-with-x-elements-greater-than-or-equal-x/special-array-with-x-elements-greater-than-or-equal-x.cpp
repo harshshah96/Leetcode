6b3ba519e84001214c2d890002f4d28c052dc329
Harsh Shah <shahharsh987656@gmail.com>
2024-05-27 15:29:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (5.12%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -10,9 +10,7 @@ public:
         sort(rbegin(nums),rend(nums));
         int n = size(nums);
         for(int i=0;i<n;i++){
-            if(nums[i]>=(i+1)){
-                if(i==n-1 || nums[i+1]<i+1)return i+1;
-            }
+            if(nums[i]>=(i+1) && (i==n-1 || nums[i+1]<i+1))  return i+1;
         }
         return -1;
     }
