8d9cf77f9b5392408d9a0369b5178a172b6a1a1b
Harsh Shah <shahharsh987656@gmail.com>
2024-06-05 15:10:46 +0530

    Time: 10 ms (44.21%) | Memory: 11.4 MB (69.23%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        // Vector to store frequency of each character (26 characters in total for lowercase letters)
+        vector<int> freq(26, INT_MAX);
+
+        // Iterate over each word
+        for (const string& word : words) {
+            // Temporary vector to store frequency of characters in the current word
+            vector<int> temp(26, 0);
+            for (char c : word) {
+                temp[c - 'a']++;
+            }
+
+            // Update the global frequency array with the minimum frequency
+            for (int i = 0; i < 26; i++) {
+                freq[i] = min(freq[i], temp[i]);
+            }
+        }
+
+        // Prepare the result
+        vector<string> result;
+        for (int i = 0; i < 26; i++) {
+            while (freq[i] > 0) {
+                result.push_back(string(1, i + 'a'));
+                freq[i]--;
+            }
+        }
+
+        return result;
+    }
+};
