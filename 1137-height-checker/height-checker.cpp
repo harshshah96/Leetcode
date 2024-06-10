d49407a1869dd4b6f0580fddba41ff5cb52e99ed
Harsh Shah <shahharsh987656@gmail.com>
2024-06-10 12:03:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (44.04%) - LeetSync

---
+++ 1137-height-checker/height-checker.cpp
@@ -0,0 +1,21 @@
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> temp = heights ; 
+
+        sort(temp.begin() ,  temp.end());
+
+        int count = 0;
+
+        for(int i=0; i< heights.size(); i++){
+            if(heights[i]!=temp[i]) count++;
+        }
+
+        return count;
+
+    }
+};
