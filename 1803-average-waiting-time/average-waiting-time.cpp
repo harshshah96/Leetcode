63188a40fc8037212af2b23cd0c2fb7cde17fb6f
Harsh Shah <shahharsh987656@gmail.com>
2024-07-09 23:04:10 +0530

    Time: 103 ms (100.00%) | Memory: 76.7 MB (22.35%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -1,20 +1,46 @@
-class Solution {
-public:
-    double averageWaitingTime(vector<vector<int>>& customers) {
-        double total_waiting_time = 0;
-        int current_time = 0;
+static int io_opt = []() {
+  ios::sync_with_stdio(false);
+  return 0;
+}();
+class Solution{
+    public:
+        double averageWaitingTime( vector <vector<int>> & customers){
+            double totalWaitingTime = 0;
+            int currentTime = 0;
 
-        for (const auto& customer : customers) {
-            int arrival = customer[0];
-            int service = customer[1];
-            if (current_time < arrival) {
-                current_time = arrival;
+            for(const auto& customer : customers){
+                int arrival = customer[0];
+                int service = customer [1];
+                if(currentTime < arrival)  currentTime = arrival;
+
+                int waitingTime = currentTime - arrival + service;
+                totalWaitingTime += waitingTime;
+                currentTime +=service;
             }
-            int waiting_time = current_time - arrival + service;
-            total_waiting_time += waiting_time;
-            current_time += service;
-        }
 
-        return total_waiting_time / customers.size();
-    }
+            return (double)(totalWaitingTime / customers.size());
+
+        }
 };
+
+
+// class Solution {
+// public:
+//     double averageWaitingTime(vector<vector<int>>& customers) {
+//         double total_waiting_time = 0;
+//         int current_time = 0;
+
+//         for (const auto& customer : customers) {
+//             int arrival = customer[0];
+//             int service = customer[1];
+//             if (current_time < arrival) {
+//                 current_time = arrival;
+//             }
+//             int waiting_time = current_time - arrival + service;
+//             total_waiting_time += waiting_time;
+//             current_time += service;
+//         }
+
+//         return total_waiting_time / customers.size();
+//     }
+// };
